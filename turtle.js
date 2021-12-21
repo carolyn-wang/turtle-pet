0a924e35f4604447ea7844e0c407f6cad19da471
Carolyn Wang <carolynw@berkeley.edu>
2021-12-21 00:56:39 -0800

    first real commit

---
+++ turtle.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function() {
+
+  var mouseX = 0, mouseY = 0;
+  var xp = 0, yp = 0;
+   
+  $(document).mousemove(function(e){
+    mouseX = e.pageX - 30;
+    mouseY = e.pageY - 30; 
+  });
+    
+  setInterval(function(){
+    xp += ((mouseX - xp)/800);
+    yp += ((mouseY - yp)/800);
+    $("#circle").css({left: xp +'px', top: yp +'px'});
+  }, 2);
+
+});
